8f676845ac7278b827ba9eae0c8ed443c161cdf9
Reniperei <116393275+Reniperei@users.noreply.github.com>
2022-11-03 20:44:41 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title('naive bayes IA iris')
 st.write ('criando IA iris')
 
-from sklearn.ensemble import Randonforestclassifier
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd #importa dados csv
 dados=pd.read_csv('/content/drive/MyDrive/IA claudio/Iris - Iris (1).csv')
