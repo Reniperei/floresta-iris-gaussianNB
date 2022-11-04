6e774c4445c503697c4356fbb8ac36ff41b95825
Reniperei <116393275+Reniperei@users.noreply.github.com>
2022-11-04 15:02:17 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -71,10 +71,8 @@ def iris_example():
   # Train the classifier
  classifier.fit(iris_features_train, iris_species_train)
  
-
-  fig.show()
                
-     
+    
   if button_col:
     run_button = st.button('Run Code', key=run_button_key, on_click=button_created(run_button_key))
     st.subheader('Output:')
