8a02cd0def3a56153c8a70eacc70ca5faf607796
Reniperei <116393275+Reniperei@users.noreply.github.com>
2022-11-04 14:53:08 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -8,6 +8,32 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+from sklearn.datasets import load_iris
+iris = load_iris()
+  
+# store the feature matrix (X) and response vector (y)
+X = iris.data[:6]
+y = iris.target[:6]
+print("X:", X)
+print("Y:",y)
+
+# splitting X and y into training and testing sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.6, random_state=4)
+  
+# training the model on training set
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+  
+# making predictions on the testing set
+y_pred = gnb.predict(X_test)
+  
+# comparing actual response values (y_test) with predicted response values (y_pred)
+from sklearn import metrics
+print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, y_pred)*100)
+
 # Import function to fetch dataset
 from sklearn.datasets import load_iris
 
