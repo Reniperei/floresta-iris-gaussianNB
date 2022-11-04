b8ca1288b9274f6878086e5209e0c172b6f9236f
Reniperei <116393275+Reniperei@users.noreply.github.com>
2022-11-03 22:10:09 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -15,6 +15,36 @@ st.experimental_show
 st.pyplot
 st.code
 
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd 
+
+                importando os dados do csv                          
+
+dados = pd.read_csv('/content/drive/MyDrive/IA claudio/Iris - Iris (1).csv')
+           separar as classes das features                         
+classes = dados['Species']
+nomesColunas = dados.columns.to_list()
+tamanho = len(nomesColunas)#quantos nomes tem
+nomesColunas = nomesColunas[1:tamanho-1]
+features = dados[nomesColunas]#monta o features
+           quebrar os dados em teste e treino                      
+from sklearn.model_selection import train_test_split
+
+features_treino,features_teste,classes_treino,classes_teste = train_test_split(features,
+                                                                               classes,
+                                                                               test_size=0.4,
+                                                                               random_state=2)
+
+floresta = RandomForestClassifier(n_estimators=1000) 
+
+floresta.fit(features_treino,classes_treino)
+
+predicoes = floresta.predict(features_teste)
+from sklearn import metrics
+
+print(metrics.classification_report(classes_teste,predicoes))
+
+
 from sklearn.naive_bayes import GaussianNB 
 import numpy as np  #importa dados csv
 dados=pd.read_csv('/content/drive/MyDrive/naive bayes2.ipynb')
