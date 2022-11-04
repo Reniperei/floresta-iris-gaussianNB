b443c4fb6a0dc3ecc3323d9b0d0e724f40b3895b
Reniperei <116393275+Reniperei@users.noreply.github.com>
2022-11-03 21:19:39 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -7,3 +7,5 @@ from sklearn.naive_bayes import GaussianNB
 import numpy as np  #importa dados csv
 dados=pd.read_csv('/content/drive/MyDrive/IA claudio/Iris - Iris (1).csv')
 st.code #importa dados csv
+
+  
