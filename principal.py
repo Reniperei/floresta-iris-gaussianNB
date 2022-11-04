31d445620258aca7e637f467e9d5b10263e374d8
Reniperei <116393275+Reniperei@users.noreply.github.com>
2022-11-03 22:50:09 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -15,65 +15,30 @@ st.experimental_show
 st.pyplot
 st.code
 
-from sklearn.ensemble import RandomForestClassifier
-import pandas as pd 
+from sklearn.datasets import load_iris
+iris = load_iris()
+  
+# store the feature matrix (X) and response vector (y)
+X = iris.data[:6]
+y = iris.target[:6]
+print("X:", X)
+print("Y:",y)
 
-                importando os dados do csv                          
-
-dados = pd.read_csv('/content/drive/MyDrive/IA claudio/Iris - Iris (1).csv')
-           separar as classes das features                         
-classes = dados['Species']
-nomesColunas = dados.columns.to_list()
-tamanho = len(nomesColunas)#quantos nomes tem
-nomesColunas = nomesColunas[1:tamanho-1]
-features = dados[nomesColunas]#monta o features
-           quebrar os dados em teste e treino                      
+# splitting X and y into training and testing sets
 from sklearn.model_selection import train_test_split
-
-features_treino,features_teste,classes_treino,classes_teste = train_test_split(features,
-                                                                               classes,
-                                                                               test_size=0.4,
-                                                                               random_state=2)
-
-floresta = RandomForestClassifier(n_estimators=1000) 
-
-floresta.fit(features_treino,classes_treino)
-
-predicoes = floresta.predict(features_teste)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.6, random_state=4)
+  
+# training the model on training set
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+  
+# making predictions on the testing set
+y_pred = gnb.predict(X_test)
+  
+# comparing actual response values (y_test) with predicted response values (y_pred)
 from sklearn import metrics
-
-print(metrics.classification_report(classes_teste,predicoes))
-
-
-from sklearn.naive_bayes import GaussianNB 
-import numpy as np  #importa dados csv
-dados=pd.read_csv('/content/drive/MyDrive/IA claudio/Iris - Iris (1).csv')
-#importa a biblioteca do modelo Naive Bayes Gaussiano
-#import pandas as pd #manipulacao de dados
-from sklearn.naive_bayes import  GaussianNB
-import numpy as np
-
-
-x = np.array([[1,2],[1,2],[1,2],[-2,0],[2,3],[-4,0],[-1,1],[1,2],[-2,2],[2,7],[-4,1],[0,0]])
-y = np.array([10, 10, 15, 4, 3, 10, 3, 15, 4, 3, 4,6 ])
-
-
-# splittin X and y into training and testing sets
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)
-
-#cria um classificador Gaussiano
-model = GaussianNB()
-
-#treina o modelo usando os dandos de treino
-model.fit(x_train, y_train)
-
-#resultado de previsão
-
-teste = np.array([[-1,1],[1,2],[-2,3],[-3,6]])
-#predicted = model.predict([[1,2][0,0]]) #fazer a previsão em cima desses 2 numeros
-predicted = model.predict(teste) #fazer a previsão em cima desse array teste
-print(predicted)
+print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, y_pred)*100)
 
 
 
