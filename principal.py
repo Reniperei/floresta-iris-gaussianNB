4cec321242314abebe484366a0b35579bb282d80
Reniperei <116393275+Reniperei@users.noreply.github.com>
2022-11-03 21:59:11 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -2,10 +2,24 @@ import streamlit as st
 
 st.title('naive bayes IA iris')
 st.write ('criando IA iris')
+st.number_input
+st.text_input
+st.file_uploader
+st.image
+st.columns
+st.tabs
+st.container
+st.dataframe
+st.metric
+st.experimental_show
+st.pyplot
+st.code
 
 from sklearn.naive_bayes import GaussianNB 
 import numpy as np  #importa dados csv
-dados=pd.read_csv('/content/drive/MyDrive/IA claudio/Iris - Iris (1).csv')
-st.code #importa dados csv
+dados=pd.read_csv('/content/drive/MyDrive/naive bayes2.ipynb')
+
+  
+
 
   
