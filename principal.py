a5f0e6949ae2f8e90878634eb51cd179da0e6bb1
Reniperei <116393275+Reniperei@users.noreply.github.com>
2022-11-07 20:41:58 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -20,38 +20,9 @@ predicoes = floresta.predict(features_teste)
 from sklearn import metrics 
 print(metrics.classification_report(classes_teste,predicoes))
 
-                                                                              
-  
-# comparing actual response values (y_test) with predicted response values (y_pred)
-from sklearn import metrics
-print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, y_pred)*100)
+                              
 
-# Import function to fetch dataset
-from sklearn.datasets import load_iris
 
-# Import Multinomial Naive-Bayes classifier
-from sklearn.naive_bayes import GaussianNB
-
-# Import train_test_split
-from sklearn.model_selection import train_test_split
-
-# Import sklearn metrics for analysis
-from sklearn.metrics import classification_report, confusion_matrix
-
-def iris_example():
-
- 
- from sklearn.datasets import load_iris
- # Load the data
- iris_df = load_iris(as_frame=True)
- iris_df.target.replace({0: 'setosa', 1: 'versicolor', 2: 'virginica'}, inplace=True)
- # Split the data
- iris_features_train, iris_features_test, iris_species_train, iris_species_test = train_test_split(iris_df.data, iris_df.target, train_size=0.6, shuffle=True, random_state=42)
-   # Instantiate a classifier
- classifier = GaussianNB()
-                
-  # Train the classifier
- classifier.fit(iris_features_train, iris_species_train)
 
  
 
