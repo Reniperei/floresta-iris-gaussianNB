769dade78645de2a8bf1ad0d7d0e43009df843b4
Reniperei <116393275+Reniperei@users.noreply.github.com>
2022-11-07 20:55:51 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -13,12 +13,20 @@ features=dados[nomesColunas]
 from sklearn.model_selection import train_test_splitfeatures_treino,features_teste,classes_treino,classes_teste= train_test_split(features,classes,test_size=0,26,randon_state=3)
 model=GaussianNB()
 
-floresta = RandomForestClassifier(n_estimators=90) 
+treina o modelo usando os dandos de treino
+model.fit(x_train, y_train)
 
-floresta.fit(features_treino,classes_treino)
-predicoes = floresta.predict(features_teste)
-from sklearn import metrics 
-print(metrics.classification_report(classes_teste,predicoes))
+#resultado de previsão
+
+teste = np.array([[-4,0],[0,0],[-2,0],[2,7]])
+#predicted = model.predict([[1,2][0,0]]) #fazer a previsão em cima desses 2 numeros
+predicted = model.predict(teste) #fazer a previsão em cima desse array teste
+print(predicted)
+
+#comparing actual response values (y_test) with predicted response values (y_pred)
+from sklearn import metrics
+print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, predicted)*100)
+
 
 
                               
