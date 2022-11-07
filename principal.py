1b9e4ccba12104ff755b14b518bc12693c625b2a
Reniperei <116393275+Reniperei@users.noreply.github.com>
2022-11-07 20:57:56 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -10,8 +10,7 @@ nomesColunas=dados.columns.to_list()
 tamanho=len(nomesColunas)
 nomesColunas= nomesColunas[1:tamanho-1]
 features=dados[nomesColunas]
-from sklearn.model_selection import train_test_splitfeatures_treino,features_teste,classes_treino,classes_teste= train_test_split(features,classes,test_size=0,26,randon_state=3)
-model=GaussianNB()
+from sklearn.model_selection import train_test_splitfeatures_treino,features_teste,classes_treino,classes_teste= train_test_split(features,classes,test_size=0,26,randon_state=3)model=GaussianNB()
 
 treina o modelo usando os dandos de treino
 model.fit(x_train, y_train)
